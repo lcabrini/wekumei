969fbedd6033e1faaa489194d43e14820bb0db94
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-20 10:40:25 +0000

    Initial work on Person model.

--- family/models.py
+++ family/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class Person(models.Model):
-    pass
+    family_name = models.CharField(max_length=100)
+    maiden_name = models.CharField(max_length=100)
+    given_name = models.CharField(max_length=100)
+    other_names = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{}, {}".format(self.family_name, self.first_name)
 
